40e71810d613fd27308faa6b3550b926284cd063
Andys-in <andyjeffersonperez@gmail.com>
2023-01-19 21:46:48 -0500

    adding remaining stub routes

---
+++ README.md
@@ -0,0 +1,3 @@
+# Proyect Rest-Aurant 
+
+REST-AURANT is an app where useres can reviwe restaurants 
--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+// require('dotenv').config()
+// const express = require('express')
+// const app = express()
+
+// app.use('/places', require('./controllers/places'))
+
+// app.get('/', (req, res) => {
+//     res.send('Hello world!')
+// })
+
+// app.get('*', (req, res) =>{
+//     res.status(404) .send('<h1>404 Page</h1>')
+// })
+
+// app.listen(procces.env.PORT)
 require('dotenv').config()
 const express = require('express')
 const app = express()
@@ -8,8 +23,9 @@ app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.get('*', (req, res) =>{
-    res.status(404) .send('<h1>404 Page</h1>')
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
 })
 
-app.listen(procces.env.PORT)
+app.listen(process.env.PORT)
+
