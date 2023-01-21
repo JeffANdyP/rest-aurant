32607ccf97e489f50979bc32b0d57e72a6741ceb
Andys-in <andyjeffersonperez@gmail.com>
2023-01-21 13:30:06 -0500

    adding the styles to Rest-aurant

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+// Modules and Globals
 require('dotenv').config()
 const express = require('express')
 const app = express()
 
+// express settings
+ 
+// app.set('views', _dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+// Controllers & Routes
 app.use('/places', require('./controllers/places'))
-
 
 app.get('/', (req, res) => {
  res.render('home')
@@ -16,4 +21,5 @@ app.get('*', (req, res) => {
   res.render('error404')
 })
 
+// Listen for Connections
 app.listen(process.env.PORT)
--- views/home.jsx
+++ views/home.jsx
@@ -1,17 +1,19 @@
- const React = require('react')
+const React = require('react')
 const Def = require('./default')
 
-function home () {
-    return (
-      <Def>
-          <main>
-              <h1>HOME</h1>
-          </main>
-      </Def>
-    )
-  }
+function home() {
+  return (
+    <Def>
+      <main>
+        <h1>HOME</h1>
+        <a href="/places">
+          <button className="btn-primary">Places Page</button>
+        </a>
+      </main>
+    </Def>
+  )
+}
 
 
 module.exports = home
 
-  
