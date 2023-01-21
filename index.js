68ca285a57c98a93bd9b2582470d25cdbd8afd5f
Andys-in <andyjeffersonperez@gmail.com>
2023-01-21 10:54:30 -0500

    adding default viwe for layout

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-// require('dotenv').config()
-// const express = require('express')
-// const app = express()
-
-// app.use('/places', require('./controllers/places'))
-
-// app.get('/', (req, res) => {
-//     res.send('Hello world!')
-// })
-
-// app.get('*', (req, res) =>{
-//     res.status(404) .send('<h1>404 Page</h1>')
-// })
-
-// app.listen(procces.env.PORT)
 require('dotenv').config()
 const express = require('express')
 const app = express()
